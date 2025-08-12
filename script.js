e570b506a5a1b06bcee5c8f69d9e5bf94b2c723f
sharath pillai <spillai.developer@gmail.com>
2025-08-12 13:18:51 +0300

    updated html,css&javascript

---
+++ script.js
@@ -0,0 +1,64 @@
+const darkMode = document.querySelector('.dark-mode');
+const pokemonContainer = document.querySelector('.pokemon-container');
+
+
+darkMode.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode-active');
+    if (document.body.classList.contains('dark-mode-active')) {
+        darkMode.innerHTML = '<i class="fa-solid fa-sun"></i> Light Mode'
+        darkMode.style.color = 'white';
+    } else {
+        darkMode.innerHTML = '<i class="fa-solid fa-moon"></i> Dark Mode'
+        darkMode.style.color = 'black';
+    }
+})
+
+
+
+async function fetchCountries() {
+    try {
+        const data = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151');
+        const pokemon = await data.json()
+        const pokemonData = pokemon.results;
+        showPokemon(pokemonData);
+    } catch (err) {
+        console.error('Error fetching pokemon:', err)
+    }
+}
+fetchCountries();
+
+
+
+function showPokemon(pokemonData) {
+    // console.log(pokemonData)
+    pokemonData.forEach((data) => {
+        // console.log(data)
+        async function fetchPokemonDetails(name) {
+            try{
+                const deatails=await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
+                const pokemonDetails = await deatails.json();
+                // console.log(pokemonDetails.sprites.front_default);
+                getPokemonImage(pokemonDetails, data);
+            }catch (error) {
+                console.error('Error fetching Pokemon details:', error);
+            }
+        }
+        fetchPokemonDetails(data.name)
+
+        function getPokemonImage(pokemonDetails,data) {
+            // console.log(pokemonDetails.sprites.front_default);
+            // console.log(data.name)
+            const pokemonCard = document.createElement('a')
+            pokemonCard.classList.add('pokemon-card')
+    
+            const cardHTML = `
+                <img class="pokemon-img" src="${pokemonDetails.sprites.front_default}" alt="${data.name}">
+                <div class="details">
+                    <h3 class="pokemon-name">${data.name}</h3>
+                </div>
+            `
+            pokemonCard.innerHTML = cardHTML
+            pokemonContainer.append(pokemonCard)
+        }
+    });
+}
